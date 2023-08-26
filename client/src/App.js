cff831574339efbd87a7aaa6aaf4bce483ccede7
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-08-26 18:25:26 +0530

    normal changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import { Login , SignUP } from "./pages/auth/auth";
 import { Home } from "./pages/home/home";
 import { Locate } from './pages/locate/locate';
-import { News } from "./components/AdiNews";
 
 // components
 import { Navbar } from "./components/navbar";
@@ -22,7 +21,6 @@ function App() {
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<SignUP/>}/>
               <Route path='/locate' element={<Locate/>}/>
-              <Route path="/news" element={<News/>}/>
 
             </Routes>
 
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,9 +1,7 @@
 import "./home.css";
 
-import {Link} from "react-router-dom";
-
 export const Home=()=>{
     return(<div className="page-body">
-        <Link to={"/news"}><button>NEWS</button></Link>
+        HOME PAGE
     </div>)
 };
