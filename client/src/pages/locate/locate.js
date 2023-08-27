2a72172f9d03717ec1316e9ea6455a47d484f59a
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-08-27 19:20:51 +0530

    structing in Locate

--- client/src/pages/locate/locate.js
+++ client/src/pages/locate/locate.js
@@ -1,7 +1,51 @@
 import "./locate.css";
 
 export const Locate=()=>{
-    return(<div className="page-body">
-        Locate
-    </div>)
+    return(<>
+        <div className="main-section">
+
+            {/* LEFT */}
+            <div className="section">
+
+                <div className="left-back">
+
+                </div>
+
+                <div className="left-boxes">
+
+                </div>
+
+                <div className="left-front">
+
+                </div>
+
+            </div>
+            {/* LEFT */}
+            
+
+
+
+            {/* Right */}
+            <div className="section">
+
+                <div className="right-up">
+                    
+                </div>
+
+                <div className="right-down">
+
+                    <div className="right-table">
+
+                    </div>
+
+                    <div className="right-wall">
+
+                    </div>
+
+                </div>
+            </div>
+            {/* Right */}
+
+        </div>
+    </>)
 }
