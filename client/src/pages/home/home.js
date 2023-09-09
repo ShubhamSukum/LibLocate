2a9e691ac390b26baebd445af96f30ff81d5519c
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-09 21:35:25 +0530

    changing file uploading way

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -3,83 +3,15 @@ import axios from 'axios';
 import "./home.css";
 
 export const Home = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [image, setImage] = useState(null);
     const [account, setAccount] = useState('novone2020');
-
-    const [postData, setPostData] = useState([]);
-
-    const arrayBufferToBase64 = (buffer, mimeType) => {
-        console.log(buffer)
-        let binary = '';
-    let bytes = new Uint8Array(buffer.data);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-      binary += String.fromCharCode(bytes[i]);
-    }   
-    return binary.toString("base64")
-    }
-
-    useEffect(() => {
-        axios.get("http://localhost:3001/pict0/getPost").
-            then((res) => {
-                setPostData(
-                    res.data.data.map(val => {
-                        console.log(val)
-                        return {
-                            ...val,
-                            image: arrayBufferToBase64(res.data.data[0].image.data, res.data.data[0].image.contentType)
-                        }
-                    }),
-                );
-                console.log(postData[0].image)
-                // console.log(arrayBufferToBase64(res.data.data[0].image.data))
-            }).catch((err) => {
-                console.error(err);
-            });
-    }, []);
-
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                setImage(event.target.result);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const [postData, setPostData] = useState({
+        title:"",
+        description:"",
+        image:"",
+        account:""
+    });
     
-        const formData = new FormData();
-        formData.append('title', title);
-        formData.append('description', description);
     
-        if (image) {
-            const imageBlob = new Blob([image], { type: 'image/jpeg' }); // Adjust the MIME type accordingly
-            formData.append('image', imageBlob);
-        }
-    
-        formData.append('account', account);
-    
-        try {
-            await axios.post('http://localhost:3001/pict0/createPost', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            });
-            console.log('Post created successfully');
-            // Reset form fields if needed
-        } catch (error) {
-            console.error('Error creating post:', error);
-        }
-    };
-    
-
     return (
         <div className="home-screen">
             <div className="home-post-area">
@@ -121,124 +53,73 @@ export const Home = () => {
 };
 
 
-// import React, { useState,useEffect } from 'react';
-// import axios from 'axios';
-// import "./home.css";
-
-// export const Home=()=>{
-//     const [title, setTitle] = useState('');
-//     const [description, setDescription] = useState('');
-//     const [image, setImage] = useState(null);
-//     const [account, setAccount] = useState('novone2020');
 
 
-//     // setAccount("asd")
+// const arrayBufferToBase64 = (buffer, mimeType) => {
+    //     console.log(buffer)
+    //     let binary = '';
+    //     let bytes = new Uint8Array(buffer.data);
+    //     let len = bytes.byteLength;
+    //     for (let i = 0; i < len; i++) {
+    //     binary += String.fromCharCode(bytes[i]);
+    //     }   
+    //     return binary.toString("base64")
+    // }
 
-//     // fetching posts
-//     const [postData,setPostData]=useState([]);
+    // useEffect(() => {
+    //     axios.get("http://localhost:3001/pict0/getPost").
+    //         then((res) => {
+    //             setPostData(
+    //                 res.data.data.map(val => {
+    //                     console.log(val)
+    //                     return {
+    //                         ...val,
+    //                         image: arrayBufferToBase64(res.data.data[0].image.data, res.data.data[0].image.contentType)
+    //                     }
+    //                 }),
+    //             );
+    //             console.log(postData[0].image)
+    //             // console.log(arrayBufferToBase64(res.data.data[0].image.data))
+    //         }).catch((err) => {
+    //             console.error(err);
+    //         });
+    // }, []);
 
-//     useEffect(()=>{
-//         axios.get("http://localhost:3001/pict0/getPost").
-//         then((res)=>{
-//             setPostData(res.data.data);
-//             // console.log(res.data.data[0])
-//         }).catch((err)=>{
-//             console.error(err);
-//         })
+    // const handleFileChange = (e) => {
+    //     const file = e.target.files[0];
 
-//         console.log("effect")
-//     },[])
+    //     if (file) {
+    //         const reader = new FileReader();
+    //         reader.onload = (event) => {
+    //             setImage(event.target.result);
+    //         };
+    //         reader.readAsDataURL(file);
+    //     }
+    // };
 
-//     const handleFileChange = (e) => {
-//         const file = e.target.files[0];
-
-//         if (file) {
-//             const reader = new FileReader();
-//             reader.onload = (event) => {
-//                 setImage(event.target.result);
-//             };
-//             reader.readAsDataURL(file);
-//         }
-//     };
-
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
     
-//         const formData = new FormData();
-//         formData.append('title', title);
-//         formData.append('description', description);
-//         formData.append('image', image);
-//         formData.append('account', account);
+    //     const formData = new FormData();
+    //     formData.append('title', title);
+    //     formData.append('description', description);
     
-//         try {
-//           await axios.post('http://localhost:3001/pict0/createPost', formData, {
-//             headers: {
-//               'Content-Type': 'multipart/form-data'
-//             }
-//           });
-//           console.log('Post created successfully');
-//           // Reset form fields if needed
-//         } catch (error) {
-//           console.error('Error creating post:', error);
-//         }
-//     };
-
-//     return(<div className="home-screen">
-//         <div className="home-post-area">
-//             {/* {
-//                 postData.map((data, index) => {
-//                     return (
-//                         <div key={index}>
-//                             <img src={data.image} alt={data.title} />
-//                             <p>{data.title}</p>
-//                             <p>{data.description}</p>
-//                             <p>{data.account}</p>
-                            
-//                         </div>
-//                     )
-//                 })
-//             } */}
-
-//             {
-//                 postData.map((data, index) => {
-//                     // const image = `data:${data.image.contentType};base64,${Buffer.from(data.image.data).toString('base64')}`;
-//                     return (
-//                         <div key={index}>
-//                             <img src={image} alt="Selected" />
-//                             <p>{data.title}</p>
-//                             <p>{data.description}</p>
-//                             <p>{data.account}</p>
-//                         </div>
-//                     )
-//                 })
-//             }
-//         </div>
-
-//         <div className="home-share-area">
-//             <h3 style={{color:"white",marginTop:"1vh"}}>SHARE</h3>
-//             <hr/>
-//             <form className="form-adjust form-control" onSubmit={handleSubmit}>
-
-//                 <h4 style={{color:"white"}}>Title</h4>
-//                 <input type="text" placeholder="Post title" className="post-title home-inputs" 
-//                 value={title} onChange={(e) => setTitle(e.target.value)}/>
-
-//                 <h4 style={{color:"white"}}>Description</h4>
-//                 <input type="text" placeholder="Post Description" className="post-description home-inputs"
-//                 value={description} onChange={(e) => setDescription(e.target.value)}/>
-
-//                 <center>
-//                     <input type="file" className="file-button btn btn-dark" 
-//                     style={{display:"block",marginBottom:"1vh"}}
-//                     accept="image/*" onChange={(e) => setImage(e.target.files[0])} />
-//                 </center>
-
-//                 <button type="submit" className="btn btn-light">Submit</button>
-//             </form>
-//         </div>
-        
-//     </div>)
-// };
-
-// css background => background: linear-gradient(rgba(220,220,255,0.4) 85%, rgba(255,255,255,0.5) 100%);
+    //     if (image) {
+    //         const imageBlob = new Blob([image], { type: 'image/jpeg' }); // Adjust the MIME type accordingly
+    //         formData.append('image', imageBlob);
+    //     }
+    
+    //     formData.append('account', account);
+    
+    //     try {
+    //         await axios.post('http://localhost:3001/pict0/createPost', formData, {
+    //             headers: {
+    //                 'Content-Type': 'multipart/form-data'
+    //             }
+    //         });
+    //         console.log('Post created successfully');
+    //         // Reset form fields if needed
+    //     } catch (error) {
+    //         console.error('Error creating post:', error);
+    //     }
+    // };
--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import multer from "multer"; 
 
 const app = express();
 
@@ -15,70 +14,13 @@ postRouter.get("/getPost", async (req, res) => {
     try {
       const postData = await postModel.find({}); // Exclude image data for now
       return res.status(200).json({ success: true, data: postData });
-    } catch (err) {
+    } 
+    catch (err) {
       console.error(err);
       return res.status(500).json({ success: false, message: "Internal server error" });
     }
 });
   
-
-// postRouter.get("/getPost", async (req, res) => {
-//   try {
-//     const postData = await postModel.find({});
-//     return res.send(postData);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-
-// Define the storage and upload middleware
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-postRouter.post('/createPost', upload.single('image'), async (req, res) => {
-  try {
-      const newPost = new postModel({
-          title: req.body.title,
-          description: req.body.description,
-          image: {
-              data: req.file.buffer,
-              contentType: req.file.mimetype
-          },
-          account: req.body.account,
-      });
-
-      await newPost.save();
-      res.json({ success: true, message: 'Image uploaded and post saved successfully.' });
-
-  } catch (error) {
-      console.error('Error uploading image:', error);
-      res.status(500).json({ success: false, message: 'Error uploading image.' });
-  }
-});
-
-
-// postRouter.post('/createPost', upload.single('image'), async (req, res) => {
-//     try {
-//         const newPost = new postModel({
-//             title: req.body.title,
-//             description: req.body.description,
-//             image: {
-//                 data: req.file.buffer,
-//                 contentType: req.file.mimetype
-//             },
-//             account: req.body.account,
-//         });
-
-//         await newPost.save();
-//         res.json({ success: true, message: 'Image uploaded and post saved successfully.' });
-
-//     } catch (error) {
-//         console.error('Error uploading image:', error);
-//         res.status(500).json({ success: false, message: 'Error uploading image.' });
-//     }
-// });
-
-
 export { postRouter };
 
 
@@ -93,7 +35,18 @@ export { postRouter };
 
 
 
+// postRouter.get("/getPost", async (req, res) => {
+// //   try {
+// //     const postData = await postModel.find({});
+// //     return res.send(postData);
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// // });
 
+// // Define the storage and upload middleware
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
 
 
