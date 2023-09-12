1b83e2c02458de4ce1dc6fee2ed3327f352a9ff7
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-13 00:27:25 +0530

    boxes Section hard coded

--- client/src/pages/locate/boxes/boxes.js
+++ client/src/pages/locate/boxes/boxes.js
@@ -1,5 +1,168 @@
+import {Link} from "react-router-dom";
+import "../locate.css";
+
 export const Boxes=()=>{
     return(<>
-        <h1>Boxes</h1>
+    <div className="centering" style={{ height: "80vh", width: "100vw" }}>
+        {/* KINDA NAV */}
+        <div className="boxex-center">
+            <div
+                style={{
+                position: "relative",
+                width: "80vw",
+                height: "8vh",
+                marginBottom: "2vh",
+                // border:"1px solid white"
+                }}
+            >
+                <Link to={"/locate"} className="btn btn-light lefty">
+                Back to Locate
+                </Link>
+                <h1 style={{ display: "inline-block", color: "white" }} className="fw-bold">
+                    BOXES
+                </h1>
+        </div>
+        {/* KINDA NAV */}
+
+            <div style={{border:"2px solid white"}}>
+            
+            <div className="boxex-main">
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
+
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+            </div>  
+
+            <div className="boxex-main">
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
+
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+            </div>
+
+            <div className="boxex-main">
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
+
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+            </div>
+
+            <div className="boxex-main">
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
+
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+            </div>
+
+            <div className="boxex-main">
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
+
+                <div className="boxex-row-up">
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                    <button className="boxex-buty">🪑</button>
+                </div>
+            </div>
+
+            </div>
+        </div>
+        </div>
     </>)
 }
