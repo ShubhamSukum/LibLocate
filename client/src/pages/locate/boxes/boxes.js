dd9f44f888be48a0bf5d99a7b35aa361c2ce616c
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-13 23:00:14 +0530

    boxes UI done IMPLEMENTATION remains

--- client/src/pages/locate/boxes/boxes.js
+++ client/src/pages/locate/boxes/boxes.js
@@ -1,167 +1,73 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import "../locate.css";
+import { useEffect, useState } from "react";
 
-export const Boxes=()=>{
-    return(<>
-    <div className="centering" style={{ height: "80vh", width: "100vw" }}>
+export const Boxes = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/pict0/boxes").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="centering" style={{ height: "88vh", width: "100vw"}}>
         {/* KINDA NAV */}
         <div className="boxex-center">
-            <div
-                style={{
-                position: "relative",
-                width: "80vw",
-                height: "8vh",
-                marginBottom: "2vh",
-                }}
-            >
-                <Link to={"/locate"} className="btn btn-light lefty">
-                Back to Locate
-                </Link>
-                <h1 style={{ display: "inline-block", color: "white" }} className="fw-bold">
-                    BOXES
-                </h1>
+          <div
+            style={{
+              position: "relative",
+              width: "80vw",
+              height: "8vh",
+              marginBottom: "2vh",
+            }}
+          >
+            <Link to={"/locate"} className="btn btn-light lefty">
+              Back to Locate
+            </Link>
+            <h1 style={{ display: "inline-block", color: "white" }} className="fw-bold">
+              BOXES
+            </h1>
+          </div>
+          {/* KINDA NAV */}
+
+          {data.map((info, index) => {
+            return info && (
+              <div style={{ border: "2px solid white" }}>
+                <div className="boxex-main">
+                  <div className="boxex-row-up">
+                    <>
+                      {info.right.map((val, ind) => {
+                        return (
+                          <>
+                            <button className="boxex-buty">🪑</button>
+                          </>
+                        );
+                      })}
+                      
+                      <center>
+                        <div style={{ border: "solid 1px white", width: "100%" }}></div>
+                      </center>
+
+                      {info.left.map((val, ind) => {
+                        return (
+                          <>
+                            <button className="boxex-buty">🪑</button>
+                          </>
+                        );
+                      })}
+                    </>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        {/* KINDA NAV */}
-
-            <div style={{border:"2px solid white"}}>
-            
-            <div className="boxex-main">
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
-
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-            </div>  
-
-            {/* <div className="boxex-main">
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
-
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-            </div> */}
-
-            {/* <div className="boxex-main">
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
-
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-            </div> */}
-
-            {/* <div className="boxex-main">
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
-
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-            </div> */}
-
-            {/* <div className="boxex-main">
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-                <center><div style={{border:"solid 1px white",width:"100%"}}></div></center>
-
-                <div className="boxex-row-up">
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                    <button className="boxex-buty">🪑</button>
-                </div>
-            </div> */}
-
-            </div>
-        </div> 
-        </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
--- client/src/pages/locate/tables/backWall.js
+++ client/src/pages/locate/tables/backWall.js
@@ -1,5 +1,44 @@
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+import "../locate.css";
+import { useEffect, useState } from "react";
+
 export const BackWall=()=>{
     return(<>
-        <h1>BackWAll Tables</h1>
+        <div className="centering" style={{ height: "80vh", width: "100vw" }}>
+
+            <div className="boxex-center">
+                <div style={{
+                    position: "relative",
+                    width: "80vw",
+                    height: "8vh",
+                    marginBottom: "2vh",
+                    }}
+                >
+                    <Link to={"/locate"} className="btn btn-light lefty">
+                    Back to Locate
+                    </Link>
+
+                    <h1 style={{ display: "inline-block", color: "white" }} className="fw-bold">
+                        BACK WALL
+                    </h1>
+                </div>
+            </div>
+            
+            <div className="table-back-wall">
+
+                <div className="upper-wall">
+
+                </div>
+
+                <div className="lower-wall">
+
+                </div>
+
+            </div>
+            
+        
+        </div>
     </>)
 };
