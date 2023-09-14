4a8b889255b2bccbcc4d416c8b103127e21ce993
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-14 21:00:41 +0530

    table structuring DONE

--- client/src/pages/locate/boxes/boxesBack.js
+++ client/src/pages/locate/boxes/boxesBack.js
@@ -10,24 +10,69 @@ export const BoxesBack=()=>{
             <div className="back-boxes">
 
                 <div className="back-boxes-left-section">
+
                     <div className="back-boxes-top">
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                        <button className="ok back-wall-but">🪑</button>
+                    </div>
+
+                    <div className="back-boxes-left">
+                        <button className="ok1 back-wall-but">🪑</button>
+                        <button className="ok1 back-wall-but">🪑</button>
+                        <button className="ok1 back-wall-but">🪑</button>
+                        <button className="ok1 back-wall-but">🪑</button>
+                        <button className="ok1 back-wall-but">🪑</button>
+                        <button className="ok1 back-wall-but">🪑</button>
+                    </div>
+
+                    <div className="back-boxes-center">
+
+                        <div className="single-table">
+                            <button className="right1 chair">🪑</button>
+                            <button className="top1 chair">🪑</button>
+                            <button className="left1 chair">🪑</button>
+                            <button className="bottom1 chair">🪑</button>
+                            <div className="center-table"></div>
+                        </div>
+
+                        <div className="single-table">
+                            <button className="right1 chair">🪑</button>
+                            <button className="top1 chair">🪑</button>
+                            <button className="left1 chair">🪑</button>
+                            <button className="bottom1 chair">🪑</button>
+                            <div className="center-table"></div>
+                        </div>
+
+                        <div className="single-table">
+                            <button className="right1 chair">🪑</button>
+                            <button className="top1 chair">🪑</button>
+                            <button className="left1 chair">🪑</button>
+                            <button className="bottom1 chair">🪑</button>
+                            <div className="center-table"></div>
+                        </div>
+
+                        <div className="single-table">
+                            <button className="right1 chair">🪑</button>
+                            <button className="top1 chair">🪑</button>
+                            <button className="left1 chair">🪑</button>
+                            <button className="bottom1 chair">🪑</button>
+                            <div className="center-table"></div>
+                        </div>
 
                     </div>
 
-                    {/* <div className="inner-left-section"> */}
-                        <div className="back-boxes-left">
-
-                        </div>
-
-                        <div className="back-boxes-center">
-
-                        </div>
-                    {/* </div> */}
-                
                 </div>
 
                 <div className="back-boxes-right-section">
-
+                        <button className="back-wall-but">🪑</button>
+                        <button className="back-wall-but">🪑</button>
+                        <button className="back-wall-but">🪑</button>
                 </div>
                 
             </div>
