42105868a80b457a6ce86631f245e1584ac3cf27
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-15 12:20:04 +0530

    Non-Electric static code

--- client/src/pages/locate/tables/NonElectric.js
+++ client/src/pages/locate/tables/NonElectric.js
@@ -7,7 +7,53 @@ export const NonElectric=()=>{
             <CommonNav name="NON ELECTRIC TABLES"/>
             
             <div className="nonELEC-area-out">
-                <div className="Lblue nonELEC-area"></div>
+                <div className="Lblue nonELEC-area">
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                </div>
+
+                {/* INFO PART */}
                 <div className="nonELEC-area-info">
                     <div className="info-up centering">
                         <h1>UPPER PART</h1>
@@ -16,7 +62,47 @@ export const NonElectric=()=>{
                         <h1>LOWER PART</h1>
                     </div>
                 </div>
-                <div className="Lgreen nonELEC-area"></div>
+                {/* INFO PART */}
+
+                <div className="Lgreen nonELEC-area">                    
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="elec-table">
+                            <button className="R elec-but">🪑</button>
+                            <button className="T elec-but">🪑</button>
+                            <button className="L elec-but">🪑</button>
+                            <button className="D elec-but">🪑</button>
+                            <div className="middle-box"></div>
+                        </div>
+                        <div className="p25">
+                            <div className="front-row">
+                                <button className="fr buty">🪑</button>
+                                <button className="fr buty">🪑</button>
+                                <button className="fr buty">🪑</button>
+                                <button className="fr buty">🪑</button>
+                                <button className="fr buty">🪑</button>
+                            </div>
+                        </div>
+                </div>
             </div>
         </div>
     </>)
