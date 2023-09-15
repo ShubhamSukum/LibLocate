4d95c8990b0e356cfbdad846f66dd2f59d1f9be3
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-09-15 10:58:44 +0530

    Basic Structure for non-electric tables

--- client/src/pages/locate/tables/NonElectric.js
+++ client/src/pages/locate/tables/NonElectric.js
@@ -6,12 +6,18 @@ export const NonElectric=()=>{
         <div className="centering" style={{ height: "87vh", width: "100vw" }}>
             <CommonNav name="NON ELECTRIC TABLES"/>
             
-            <div className="nonELEC-area">
-                <div className="nonELEC-area-right"></div>
-                <div className="nonELEC-area-info"></div>
-                <div className="nonELEC-area-left"></div>
+            <div className="nonELEC-area-out">
+                <div className="Lblue nonELEC-area"></div>
+                <div className="nonELEC-area-info">
+                    <div className="info-up centering">
+                        <h1>UPPER PART</h1>
+                    </div>
+                    <div className="info-down centering">
+                        <h1>LOWER PART</h1>
+                    </div>
+                </div>
+                <div className="Lgreen nonELEC-area"></div>
             </div>
         </div>
-
     </>)
 };
